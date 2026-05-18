18f7902487fc9c3dd51a9a79756550cbeba67b99
TEPPA <maksim2021bese@gmail.com>
2026-05-18 21:29:02 +0200

    fixed CORS to localhost

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app.mount("/media", StaticFiles(directory=str(MEDIA_DIR)), name="media")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://127.0.0.1:5500"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
