6411c9e1b764bb5b9e822779e70ff8087c2fba3e
Justik <iluavzume2007@gmail.com>
2026-07-09 16:26:50 +0200

    fast fix: change url path

--- frontend/app.js
+++ frontend/app.js
@@ -50,8 +50,8 @@ let isLogin = false;
 const form = document.getElementById("profileForm");
 
 //------BACKEND--------
-const BASE_URL = "http://localhost:8000";
-// const BASE_URL = "https://weather-app-production-d28f.up.railway.app";
+// const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://weather-app-production-d28f.up.railway.app";
 
 function changeModeToAdvanced() {
     if(weatherProject && weatherProject2) {
